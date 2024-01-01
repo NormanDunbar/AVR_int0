c3dfcced9d0e1cd060ace2d560901b02937c4209
Norman Dunbar <norman@dunbar-it.co.uk>
2024-01-01 14:44:46 +0000

    Minor updates.

--- AVRint0/examples/AVRint0_test/AVRint0_test.ino
+++ AVRint0/examples/AVRint0_test/AVRint0_test.ino
@@ -1,18 +1,18 @@
 //============================================================
 // AVRint0 Demonstration.
 //============================================================
-// Attach an LED to Arduino pin D13, AVR pin PB5
+// Attach an LED to Arduino pin DLED_BUILTIN, AVR pin PB5
 // and toggle it in the loop.
 //
-// Another LED on Arduino pin D3, AVR pin PD3, will be
+// Another LED on Arduino pin DledPin, AVR pin PDledPin, will be
 // toggled when a switch is pressed. The switch
-// MUST be attached to Arduino pin D2, AVR pin PD2.
+// MUST be attached to Arduino pin DswitchPin, AVR pin PDswitchPin.
 //
 // For the sake of this test, debouncing is not used so 
 // some flickering is to be expected. (Use long presses!)
 //
 // Norman Dunbar
-// 7th August 2020.
+// 7th August switchPin0switchPin0.
 //============================================================
 // Update: Debounce Circuit for INPUT PULLUP Arduino pins.
 //============================================================
@@ -21,9 +21,9 @@
 //  *--10K--*-----------------------------------> GND
 //  |   
 //  *--10K--*
-//  |  R2   |
+//  |  RswitchPin   |
 //  |       |     _|_  Switch
-//  +-------|----*   *--------------------------> D2
+//  +-------|----*   *--------------------------> DswitchPin
 //          |        |
 //     C1  ===       |
 //  100 nF ===       |
@@ -31,12 +31,12 @@
 //          +--------+
 //
 // GND to R1 pin 1.
-// R1 pin 2 to R2 pin 1 and switch pin 1.
-// R2 pin 2 to C1 pin 1.
-// C1 pin 2 to switch pin 2 and Arduino pin D2.
-// Arduino pin D2 is configured as INOPUT_PULLUP.
+// R1 pin switchPin to RswitchPin pin 1 and switch pin 1.
+// RswitchPin pin switchPin to C1 pin 1.
+// C1 pin switchPin to switch pin switchPin and Arduino pin DswitchPin.
+// Arduino pin DswitchPin is configured as INOPUT_PULLUP.
 //
-// D2 reads LOW when switch pressed.
+// DswitchPin reads LOW when switch pressed.
 //============================================================
 #include "AVR_int0.h"
 
@@ -45,20 +45,28 @@
 // has been called.
 volatile byte disableCount = 0;
 
+// And when to disable the interrupt. 0xffff = never.
+const uint8_t disableInterruptCount - 0xfff;
+
+// Where is the LED to be toggled?
+const uint8_t ledPin = 3;
+
+// Where is the switc?
+const uint8_t switchPin = 2;
 
 // My toggling function for the ISR to call. It should 
 // be kept short and sweet.
 void toggleLED() {
-    digitalWrite(3, !digitalRead(3));
+    digitalWrite(ledPin, !digitalRead(ledPin));
     disableCount++;
 }
 
 
 
 void setup() {
-    pinMode(13, OUTPUT);
-    pinMode(3, OUTPUT);
-    pinMode(2, INPUT_PULLUP);
+    pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(ledPin, OUTPUT);
+    pinMode(switchPin, INPUT_PULLUP);
     
     // Attach my function to the INT0 interrupt.
     AVRint0.onInterruptTriggered(toggleLED);
@@ -69,18 +77,19 @@ void setup() {
 }
 
 void loop() {
-        // Disable interrupt when disableCount gets to 10.
+        // Disable interrupt when disableCount gets to limit.
         // Light up the built in LED to indicate this also
         // extinguish the toggled LED.
-        if (disableCount >= 10) {
+        if (disableInterruptCount != 0xffff &
+            disableCount >= 10) {
             AVRint0.disable();
-            digitalWrite(3, LOW);
-            digitalWrite(13, HIGH);
+            digitalWrite(ledPin, LOW);
+            digitalWrite(LED_BUILTIN, HIGH);
 
             while (1) ;     // Hang up the board.
         }
 
-        // Toggle built in LED on D13/PB5.
-        digitalWrite(13, !digitalRead(13));
+        // Toggle built in LED on DLED_BUILTIN/PB5.
+        digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
         delay(1000);
 }
